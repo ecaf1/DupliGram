e47e16b12dab2677137ad6b073b4032ff30a14a4
ecaf1 <edeilsonazevedo22@gmail.com>
2024-03-03 17:07:21 -0300

    refactor: Reorganize project

--- dupligram/dupliGram.py
+++ dupligram/dupliGram.py
@@ -1,8 +1,10 @@
 import asyncio
-from tkinter import dialog
+from datetime import datetime
 
 from dynaconf import settings
 from telethon import TelegramClient
+
+from .database import DatabaseManager
 
 
 async def get_client():
@@ -13,15 +15,33 @@ async def get_client():
     return client
 
 
+async def get_stl(client: TelegramClient, chat_id, limit):
+    db_manager = DatabaseManager("dupligram.db")
+    dialogs = await client.get_messages(chat_id, limit)
+    for dialog in dialogs:
+        if getattr(dialog, "media", None):
+            message_id = dialog.id
+            file_type = dialog.media.document.mime_type
+            file_size = dialog.media.document.size
+            name = dialog.media.document.attributes[0].file_name
+            update_at = dialog.media.document.date
+            db_manager.insert_db(
+                name, file_type, file_size, update_at, message_id, chat_id
+            )
+        # TODO: salvar a mensagem no db com suas informações (características "únicas") pra depois encontrar as duplicatas
+
+        # print(name, file_type, file_size, update_at, message_id, chat_id)
+
 
 async def main():
     client = await get_client()
     print(await client.is_user_authorized())
+    await get_stl(client, -1001393256003, 3)
+    messages = await client.get_messages(
+        -1001393256003,
+        limit=3,
+    )
 
-    dialogs = await client.get_dialogs()
 
-    for dialog in dialogs:
-        print(dialog.id)
-
-
-asyncio.run(main())
+def run():
+    asyncio.run(main())
